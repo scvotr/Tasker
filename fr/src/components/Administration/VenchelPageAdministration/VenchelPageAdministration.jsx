16756e7bd6a49861274fe5b79376c04c66faa90b
Scvotr <stasforeva23@mail.ru>
2023-11-29 15:25:00 +0300

    add cmp for create new venchel

--- fr/src/components/Administration/VenchelPageAdministration/VenchelPageAdministration.jsx
+++ fr/src/components/Administration/VenchelPageAdministration/VenchelPageAdministration.jsx
@@ -1,15 +1,34 @@
-import { MainMenuAdministration } from '../MainMenuAdministration/MainMenuAdministration';
+import { MainMenuAdministration } from "../MainMenuAdministration/MainMenuAdministration";
 import "./VenchelPageAdministration.css";
+import { Modal } from "../../Modal/Modal.jsx";
+import { useState } from "react";
+import { AddNewVenchel } from "./AddNewVenchel/AddNewVenchel.jsx";
 
 export const VenchelPageAdministration = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="admin-venchel-page">
-      <div className="admin-venchel-page__container">Редактирование оборудования</div>
+      <div className="admin-venchel-page__container">
+        Редактирование оборудования
+      </div>
       <div className="admin-venchel-page__container">
         <MainMenuAdministration />
       </div>
       <div className="admin-venchel__body">
-        оборудование
+        <button onClick={openModal}>Add</button>
+        {showModal && (
+          <Modal isOpen={openModal} onClose={closeModal}>
+            <AddNewVenchel />
+          </Modal>
+        )}
       </div>
     </div>
   );
