abeca22d666d4d7473f01f9cd76106655d16bff1
Scvotr <stasforeva23@mail.ru>
2023-11-29 15:14:10 +0300

    add venchel handle routes

---
+++ bk/routes/handleVenchelRoutes.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const VenchelControler = require("../controls/Venchel/VenchelControler")
+const { handleDefaultRoute } = require("../routes/handleDefaultRoute");
+const { protectRouteTkPl } = require("../utils/protectRouteTkPl");
+
+const routeHandlers = {
+  "/venchel/addNewVenchel": VenchelControler.addNewVenchel,
+  "/venchel/updateNewVenchel": VenchelControler.updateNewVenchel,
+  "/venchel/removeNewVenchel": VenchelControler.removeNewVenchel,
+  "/venchel/getAllVenchels": VenchelControler.getAllVenchels,
+  "/venchel/getAllVenchelsByDep": VenchelControler.getAllVenchelsByDep,
+  "/venchel/getVenchelsFile": VenchelControler.getVenchelsFile,
+  "/venchel/getFullFileContent": VenchelControler.getFullFileContent,
+};
+
+const handleVenchelRoutes = async (req, res) => {
+  const { url, method } = req;
+  console.log('handleUserRoutes', url, method );
+  
+  try {
+    if (url.startsWith("/venchel")) {
+      const routeHandler = routeHandlers[url];
+      if (routeHandler) {
+        if (method === "POST") {
+          await protectRouteTkPl(routeHandler)(req, res);
+        } else if (method === "GET") {
+          await protectRouteTkPl(routeHandler)(req, res);
+        } else {
+          handleDefaultRoute(req, res);
+        }
+      } else {
+        handleDefaultRoute(req, res);
+      }
+    } else {
+      handleDefaultRoute(req, res);
+    }
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "handleVenchelRoutes - ERROR" }));
+  }
+};
+
+module.exports = { handleVenchelRoutes };
