e65e6eec0d1d55ec78ca698565de5c2abb1485d1
Scvotr <stasforeva23@mail.ru>
2023-12-07 11:13:33 +0300

    insert default data into workshops

--- bk/Database/createDatabase.js
+++ bk/Database/createDatabase.js
@@ -428,7 +428,8 @@ const createTableWorkshops = async () => {
     // Выполняем проверку наличия записей в таблице
     const rows = await queryAsyncWraper('SELECT COUNT(*) FROM workshops', 'get');
     if (rows['COUNT(*)'] === 0) { // Если записей нет, то выполняем вставку начальных значений
-      await queryAsyncWraper("INSERT INTO workshops (department_id, name) VALUES (1, 'Цех 1'), (1, 'Цех 2'), (2, 'Цех 3'), (2, 'Цех 4')", 'run');
+      // 3-Алексиков 4-Панфилово
+      await queryAsyncWraper("INSERT INTO workshops (department_id, name) VALUES (3, 'Элеватор №1'), (3, 'Элеватор №2'), (4, 'Элеватор №1'), (4, 'Элеватор №2'), (4, 'Склад №5'), (4, 'Склады')", 'run');
     }
   } catch (error) {
     console.error('createTableWorkshops ERROR: ', error);
