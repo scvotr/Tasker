2664c014d5cd9a332ccb6db6eee086eee074aed5
Scvotr <stasforeva23@mail.ru>
2023-12-07 14:32:05 +0300

    .

--- fr/src/components/Administration/VenchelPageAdministration/VenchelViewByDep/VenchelViewByDep.jsx
+++ fr/src/components/Administration/VenchelPageAdministration/VenchelViewByDep/VenchelViewByDep.jsx
@@ -1,73 +1,61 @@
 import { ButtonGroupByDep } from "./ButtonGroupByDep/ButtonGroupByDep";
 import { useEffect, useState } from "react";
-import { VenchelTableComponent } from "../VenchelTableComponent/VenchelTableComponent";
+import { VenchelTableComponent } from '../VenchelTableComponent/VenchelTableComponent'
 
-const dep_ae_1_ID = 1;
-const dep_ae_2_ID = 2;
-const dep_pe_1_ID = 3;
-const dep_pe_2_ID = 4;
-const dep_pe_3_ID = 5;
-const dep_pe_4_ID = 6;
+const dep_ae_1_ID = 1
+const dep_ae_2_ID = 2
+const dep_pe_1_ID = 3
+const dep_pe_2_ID = 4
+const dep_pe_3_ID = 5
+const dep_pe_4_ID = 6
 
 export const VenchelViewByDep = ({ dep, venchels, reRender }) => {
-  const [selectedButton, setSelectedButton] = useState("");
-  const [aaa, setAaaa] = useState({});
+  const [selectedButton, setSelectedButton] = useState('')
+  const [aaa, setAaaa] = useState({})
 
-  useEffect(() => {
-    if (dep === 3) {
-      setSelectedButton("dep_ae_1");
-    } else if (dep === 4) {
-      setSelectedButton("dep_pe_1");
+  useEffect(()=> {
+    if(dep === 3) {
+      setSelectedButton('dep_ae_1')
+    } else if( dep === 4){
+      setSelectedButton('dep_pe_1')
     }
-    const sc_1 = venchels.venchels.filter(
-      (venchel) => venchel.sector_id === dep_ae_1_ID
-    );
-    setAaaa((prev) => ({
+    const sc_1 = venchels.venchels.filter(venchel => venchel.sector_id === dep_ae_1_ID)
+    setAaaa(prev => ({
       ...prev,
       sc_1: sc_1,
-      sc_1_l: sc_1.length,
-    }));
-    const sc_2 = venchels.venchels.filter(
-      (venchel) => venchel.sector_id === dep_ae_2_ID
-    );
-    setAaaa((prev) => ({
+      sc_1_l : sc_1.length,
+    }))
+    const sc_2 = venchels.venchels.filter(venchel => venchel.sector_id === dep_ae_2_ID)
+    setAaaa(prev => ({
       ...prev,
       sc_2: sc_2,
-      sc_2_l: sc_2.length,
-    }));
-    const sc_3 = venchels.venchels.filter(
-      (venchel) => venchel.sector_id === dep_pe_1_ID
-    );
-    setAaaa((prev) => ({
+      sc_2_l : sc_2.length,
+    }))
+    const sc_3 = venchels.venchels.filter(venchel => venchel.sector_id === dep_pe_1_ID)
+    setAaaa(prev => ({
       ...prev,
       sc_3: sc_3,
-      sc_3_l: sc_3.length,
-    }));
-    const sc_4 = venchels.venchels.filter(
-      (venchel) => venchel.sector_id === dep_pe_2_ID
-    );
-    setAaaa((prev) => ({
+      sc_3_l : sc_3.length,
+    }))
+    const sc_4 = venchels.venchels.filter(venchel => venchel.sector_id === dep_pe_2_ID)
+    setAaaa(prev => ({
       ...prev,
       sc_4: sc_4,
-      sc_4_l: sc_4.length,
-    }));
-    const sc_5 = venchels.venchels.filter(
-      (venchel) => venchel.sector_id === dep_pe_3_ID
-    );
-    setAaaa((prev) => ({
+      sc_4_l : sc_4.length,
+    }))
+    const sc_5 = venchels.venchels.filter(venchel => venchel.sector_id === dep_pe_3_ID)
+    setAaaa(prev => ({
       ...prev,
       sc_5: sc_5,
-      sc_5_l: sc_5.length,
-    }));
-    const sc_6 = venchels.venchels.filter(
-      (venchel) => venchel.sector_id === dep_pe_4_ID
-    );
-    setAaaa((prev) => ({
+      sc_5_l : sc_5.length,
+    }))
+    const sc_6 = venchels.venchels.filter(venchel => venchel.sector_id === dep_pe_4_ID)
+    setAaaa(prev => ({
       ...prev,
       sc_6: sc_6,
-      sc_6_l: sc_6.length,
-    }));
-  }, [venchels]);
+      sc_6_l : sc_6.length,
+    }))
+  }, [venchels])
 
   const handleButtonClick = (button) => {
     setSelectedButton(button);
@@ -75,59 +63,17 @@ export const VenchelViewByDep = ({ dep, venchels, reRender }) => {
 
   let test = undefined;
   if (selectedButton === "dep_ae_1") {
-    test = (
-      <VenchelTableComponent
-        dep={dep}
-        sector={dep_ae_1_ID}
-        venchels={aaa.sc_1}
-        reRender={reRender}
-      />
-    );
+    test = <VenchelTableComponent  dep={dep} sector={dep_ae_1_ID} venchels={aaa.sc_1} reRender={reRender} />
   } else if (selectedButton === "dep_ae_2") {
-    test = (
-      <VenchelTableComponent
-        dep={dep}
-        sector={dep_ae_2_ID}
-        venchels={aaa.sc_2}
-        reRender={reRender}
-      />
-    );
+    test = <VenchelTableComponent  dep={dep} sector={dep_ae_2_ID} venchels={aaa.sc_2} reRender={reRender} />
   } else if (selectedButton === "dep_pe_1") {
-    test = (
-      <VenchelTableComponent
-        dep={dep}
-        sector={dep_pe_1_ID}
-        venchels={aaa.sc_3}
-        reRender={reRender}
-      />
-    );
+    test = <VenchelTableComponent  dep={dep} sector={dep_pe_1_ID} venchels={aaa.sc_3} reRender={reRender} />
   } else if (selectedButton === "dep_pe_2") {
-    test = (
-      <VenchelTableComponent
-        dep={dep}
-        sector={dep_pe_2_ID}
-        venchels={aaa.sc_4}
-        reRender={reRender}
-      />
-    );
+    test = <VenchelTableComponent  dep={dep} sector={dep_pe_2_ID} venchels={aaa.sc_4} reRender={reRender} />
   } else if (selectedButton === "dep_pe_3") {
-    test = (
-      <VenchelTableComponent
-        dep={dep}
-        sector={dep_pe_3_ID}
-        venchels={aaa.sc_5}
-        reRender={reRender}
-      />
-    );
+    test = <VenchelTableComponent  dep={dep} sector={dep_pe_3_ID} venchels={aaa.sc_5} reRender={reRender} />
   } else if (selectedButton === "dep_pe_4") {
-    test = (
-      <VenchelTableComponent
-        dep={dep}
-        sector={dep_pe_4_ID}
-        venchels={aaa.sc_6}
-        reRender={reRender}
-      />
-    );
+    test = <VenchelTableComponent  dep={dep} sector={dep_pe_4_ID} venchels={aaa.sc_6} reRender={reRender} />
   }
 
   return (
@@ -136,12 +82,14 @@ export const VenchelViewByDep = ({ dep, venchels, reRender }) => {
         currentDep={dep}
         selectedButton={selectedButton}
         handleButtonClick={handleButtonClick}
-        testLenght={aaa}
+        testLenght = {aaa}
       />
       {test}
+      
     </>
   );
 };
+
 
 // export const VenchelViewByDep = ({ dep, venchels, reRender }) => {
 //   const [selectedButton, setSelectedButton] = useState()
