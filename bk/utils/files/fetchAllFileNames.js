eb572d69941deac7e70e8e09890f580bf754e253
Scvotr <stasforeva23@mail.ru>
2023-12-13 10:21:55 +0300

    add fetchAllFileNames func

---
+++ bk/utils/files/fetchAllFileNames.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+export const fetchAllFileNames = (dataset, folder_name) => {
+  const currentDirectory = process.cwd();
+  const result = []
+
+  for(let i = 0; i < dataset.length; i++) {
+    const current_data = dataset[i]
+
+    if(current_data.file_names) {
+      current_data.file_names = current_data.file_names.split('|')
+      current_data.old_files = []
+
+      for(let j = 0; j < current_data.file_names.length; j++ ) {
+        let file_path
+        let file_content
+
+        const file_ext = path.extname(current_data.file_names[j])
+        const file_name = current_data.file_names[j]
+
+        current_data.old_files.push({
+          type: file_ext,
+          name: file_name,
+        })
+      }
+    } else {
+      current_data.file_names = []
+      current_data.files = []
+    }
+    result.push(current_data)
+  }
+  return result
+}
