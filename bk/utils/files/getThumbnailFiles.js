9806f16016154c214f634c6267747b7198edec37
Scvotr <stasforeva23@mail.ru>
2023-12-13 09:39:09 +0300

    add getThumbnailFiles func

--- bk/utils/files/getThumbnailFiles.js
+++ bk/utils/files/getThumbnailFiles.js
@@ -3,18 +3,14 @@
 */
 import { readFileAsync } from "./readFileAsync";
 
- 
-
 export const getThumbnailFiles = async (folder_id, files_names, folder_name) => {
   const currentDirectory = process.cwd();
-  const file = []
+  const filesThumbnailContent = []
 
   for (let i = 0; i < files_names.length; i++) {
     let file_path
     let file_content
-    
     const file = files_names[i]
-    // const { type, name } = file
 
     file_path = folder_name ? `${currentDirectory}/uploads/${folder_name}/${folder_id}/thumbnail_${file.name}`
                             : `${currentDirectory}/uploads/${folder_id}/thumbnail_${file.name}`
@@ -22,7 +18,14 @@ export const getThumbnailFiles = async (folder_id, files_names, folder_name) => 
     try {
       file_content = await readFileAsync(file_path)
     } catch (error) {
-      
-    }                        
+      file_path = `${currentDirectory}/uploads/default/404.jpg`
+      file_content = await readFileAsync(file_path)
+    }
+    filesThumbnailContent.push({
+      type: file.type,
+      name: file.name,
+      content: file_content,
+    })                     
   }
+  return filesThumbnailContent
 }
