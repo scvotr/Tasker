b387f5eaafc1ea0dda30694ab58fc8ca65aae0c6
Scvotr <stasforeva23@mail.ru>
2024-01-18 19:56:54 +0300

    added a layout for DocsTestForm

--- fr/src/components/User/DocsPageUser/DocsTestForm/DocsTestForm.jsx
+++ fr/src/components/User/DocsPageUser/DocsTestForm/DocsTestForm.jsx
@@ -77,43 +77,54 @@ export const DocsTestForm = () => {
     }));
   }, [currentUser]);
 
+  const handleDownload = () => {
+    // Очистка объекта Blob
+    setBlobData(null);
+    // Сброс статуса запроса
+    setReqStatus(null);
+  };
+
   return (
     <>
-      <>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Прошу предоставить мне
-            <input type="date" name="selectData" value={formData.selectData} onChange={handleGetData} />
-          </label>
-          <label> c 
-            <input type='time' min="08:00" max="18:00" step="1800" name='timeStart' value={formData.timeStart} onChange={handleGetData} required/>
-          </label>
-          <label> до 
-            <input type='time' min="08:00" max="18:00" step="1800" name='timeEnd' value={formData.timeEnd} onChange={handleGetData} required/>
-          </label>
-          <label> в счет отпуска.</label>
-          <br />
-          <br />
-          <br />
-          <label>
-            Дата
-            <input type="date" name="currentData" value={formData.currentData} onChange={handleGetData}/>
-          </label>          
-          <p>{formData.name}</p>
-          <br />
-          <br />
-
-          <button type="submit">Отправить</button>
-        </form>
-      </>
-      <div>
-        {blobData && ( // Отображаем данные только если blobData существует
-          // Если BLOB обьект
-          // <a href={URL.createObjectURL(blobData)} download="document.docx">Скачать файл</a>
-          // Если ссылка из BLOB обьекта
-          <a href={blobData} download="document.docx">Скачать</a>
-        )}
-      </div>
-    </>
+    <form onSubmit={handleSubmit} className="form-send form-send--modifier">
+      <label className="form-send__label">
+        Прошу предоставить мне &nbsp;
+        <input type="date" name="selectData" value={formData.selectData} onChange={handleGetData} />
+      </label>
+      <label className="form-send__label">
+        &nbsp;c &nbsp;
+        <input type='time' min="08:00" max="18:00" step="1800" name='timeStart' value={formData.timeStart} onChange={handleGetData} required/>
+      </label>
+      <label className="form-send__label">
+        &nbsp;до &nbsp;
+        <input type='time' min="08:00" max="18:00" step="1800" name='timeEnd' value={formData.timeEnd} onChange={handleGetData} required/>
+      </label>
+      <label className="form-send__label">
+        &nbsp;в счет отпуска.
+      </label>
+      <br />
+      <br />
+      <label className="form-send__label">
+        Дата составления: &nbsp;  
+        <input type="date" name="currentData" value={formData.currentData} onChange={handleGetData}/>
+      </label>          
+      <p>{formData.name}</p>
+      <br />
+      <br />
+  
+      <button type="submit" className="form-send__button">Отправить</button>
+    </form>
+    <div>
+      {blobData && ( // Отображаем данные только если blobData существует
+        <div className="download-link download-link--modifier">
+          {/* Если BLOB обьект */}
+          {/* <a href={URL.createObjectURL(blobData)} download="document.docx">Скачать файл</a> */}
+          {/* Если ссылка из BLOB обьекта */}
+          <a href={blobData} download="document.docx" className="download-link__link">Скачать</a>
+          <button onClick={handleDownload} className="download-link__button">Закрыть</button>
+        </div>
+      )}
+    </div>
+  </>
   );
 };
