ac2fa032e29a5869bc83f20a6b295607900816c2
Scvotr <stasforeva23@mail.ru>
2024-01-18 14:29:34 +0300

    add func fetchData for corect error work in promis

--- fr/src/components/User/TaskPageUser/ComponentsToRender/GeneralSpComponents/GeneralSpComponents.jsx
+++ fr/src/components/User/TaskPageUser/ComponentsToRender/GeneralSpComponents/GeneralSpComponents.jsx
@@ -1,8 +1,50 @@
-import './GeneralSpComponents.css'
+import { useEffect, useState } from "react";
+import "./GeneralSpComponents.css";
+import { useAuthContext } from "../../../../../context/AuthProvider";
+import { getDataFromEndpoint } from "../../../../../utils/getDataFromEndpoint";
 
 export const GeneralSpComponents = () => {
-  return(
+  const currentUser = useAuthContext();
+  const [reqStatus, setReqStatus] = useState(null);
+  const [allTasks, setAllTasks] = useState();
+  console.log(allTasks);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getDataFromEndpoint(
+          currentUser.token,
+          "/admin/getAllTasks",
+          "POST",
+          null,
+          setReqStatus
+        );
+        setAllTasks(data);
+        setReqStatus("ok");
+      } catch (error) {
+        setReqStatus(error.message); // Устанавливаем сообщение об ошибке в состояние
+      }
+    };
+    fetchData();
+  }, [currentUser]);
+
+  return (
     <>
+      {reqStatus === null ? (
+        <>Loading...</>
+      ) : (
+        <>
+          {reqStatus === "ok" ? (
+            <div>
+
+            </div>
+          ) : (
+            <div>
+              {reqStatus} {/* Отображение сообщения об ошибке */}
+            </div>
+          )}
+        </>
+      )}
     </>
-  )
-}
+  );
+};
