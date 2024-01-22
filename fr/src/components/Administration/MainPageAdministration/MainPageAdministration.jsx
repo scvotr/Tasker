9dce95e8689b53185231218ce6284974e421a264
Scvotr <stasforeva23@mail.ru>
2024-01-22 12:51:21 +0300

    edit gitignore

--- fr/src/components/Administration/MainPageAdministration/MainPageAdministration.jsx
+++ fr/src/components/Administration/MainPageAdministration/MainPageAdministration.jsx
@@ -1,7 +1,13 @@
 import './MainPageAdministration.css'
 import { MainMenuAdministration } from '../MainMenuAdministration/MainMenuAdministration';
+import { useAuthContext } from '../../../context/AuthProvider'
 
 export const MainPageAdministration = ({currentUser}) => {
+  const currtUser = useAuthContext()
+  if (!currtUser.login) {
+    return null; // Если currentUser.login не существует, вернуть null (ничего не отображать)
+  }
+  
   return (
     <div className="admin-main-page">
       <div className="admin-main-page__container">
