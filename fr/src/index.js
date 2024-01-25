c0c3a3c4a21b918f311870b8d9c5aba084435ee7
Scvotr <stasforeva23@mail.ru>
2024-01-25 18:08:47 +0300

    edit idex.css

--- fr/src/index.js
+++ fr/src/index.js
@@ -1,11 +1,10 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { AuthProvider } from './context/AuthProvider'
 import { RouterProvider } from "react-router-dom";
 import { routes } from './routes/routes';
 import reportWebVitals from './reportWebVitals';
-import ErrorBoundary from './ErrorBoundary'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
