1eff0d2fe9c910a50df10910e4c7350bb8717b41
Scvotr <stasforeva23@mail.ru>
2024-01-25 22:30:20 +0300

    edit css styles

--- fr/src/components/User/TaskPageUser/ComponentsToRender/V2UserComponents/V2UserButtonGroup/V2UserButtonGroup.jsx
+++ fr/src/components/User/TaskPageUser/ComponentsToRender/V2UserComponents/V2UserButtonGroup/V2UserButtonGroup.jsx
@@ -22,32 +22,50 @@ export const V2UserButtonGroup = ({
     {key: 'needChekTask', label: 'Исх Подтвердить', value: needApproveToCloseAppoinTasks, color: '#0099CC'},
     {key: 'closedTask', label: 'Исх Закрытые', value: closedAppointTasks, color: '#0099CC'},
     // 
-    {key: 'res_inWorkTask', label: 'Вхд. в работе', value: responsibleTasksInWork, color: '#FF78AE'},
-    {key: 'res_approved', label: 'Вхд согласованные', value: approvedResponsibleTasks, color: '#FF78AE' },
-    {key: 'res_needChekTask', label: 'Вхд на подтверждении', value: needApproveToCloseResponsibleTasks, color: '#FF78AE'},
-    {key: 'res_closedTask', label: 'Вхд закрытые', value: closedResponsibleTasks, color: '#FF78AE'},
-  ]
-
-  const inputBtn = [
     // {key: 'res_inWorkTask', label: 'Вхд. в работе', value: responsibleTasksInWork, color: '#FF78AE'},
     // {key: 'res_approved', label: 'Вхд согласованные', value: approvedResponsibleTasks, color: '#FF78AE' },
     // {key: 'res_needChekTask', label: 'Вхд на подтверждении', value: needApproveToCloseResponsibleTasks, color: '#FF78AE'},
     // {key: 'res_closedTask', label: 'Вхд закрытые', value: closedResponsibleTasks, color: '#FF78AE'},
   ]
 
+  const inputBtn = [
+    {key: 'res_inWorkTask', label: 'Вхд. в работе', value: responsibleTasksInWork, color: '#FF78AE'},
+    {key: 'res_approved', label: 'Вхд согласованные', value: approvedResponsibleTasks, color: '#FF78AE' },
+    {key: 'res_needChekTask', label: 'Вхд на подтверждении', value: needApproveToCloseResponsibleTasks, color: '#FF78AE'},
+    {key: 'res_closedTask', label: 'Вхд закрытые', value: closedResponsibleTasks, color: '#FF78AE'},
+  ]
+
   return (
     <div className="user-button-group">
-      {outBtn.map((button) => (
-        <button 
-          key={button.key}
-          onClick={()=> handleButtonClick(button.key)}
-          style={{
-            backgroundColor: selectedButton === button.key ? "green" : ""
-          }}
-        >
-          {button.label} {button.value}
-        </button>
-      ))}
+      <div className='test1'>
+
+        {outBtn.map((button) => (
+          <button 
+            className='user-task__button'
+            key={button.key}
+            onClick={()=> handleButtonClick(button.key)}
+            style={{
+              backgroundColor: selectedButton === button.key ? "green" : ""
+            }}
+          >
+            {button.label} {button.value}
+          </button>
+        ))}
+      </div>
+      <div className='test1'>
+        {inputBtn.map((button) => (
+          <button 
+            className='user-task__button'
+            key={button.key}
+            onClick={()=> handleButtonClick(button.key)}
+            style={{
+              backgroundColor: selectedButton === button.key ? "green" : ""
+            }}
+          >
+            {button.label} {button.value}
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
