ce732763b0c72244c6a8ac608b18498857a7d32d
scvotr <71510141+scvotr@users.noreply.github.com>
2024-01-27 16:51:11 +0300

    remove console.log

--- bk/Database/createDatabase.js
+++ bk/Database/createDatabase.js
@@ -147,7 +147,6 @@ const HASH_SALT = parseInt(process.env.KEY_SALT)
 const SECRET_KEY = process.env.KEY_TOKEN
 
 const queryAsyncWraper = (command, method = 'all') => {
-  // console.log('comand', command, 'method', method)
   const db = new sqlite3.Database('../database.db')
   return new Promise((resolve, reject) => {
     db[method](command, (err, result) => {
@@ -296,8 +295,8 @@ const createTableDepartments = async () => {
     if (rows['COUNT(*)'] === 0) { // Если записей нет, то выполняем вставку начальных значений
       await queryAsyncWraper("INSERT INTO departments (name) VALUES ('новый'), ('Гелио-Пакс'), ('Алексиковский Э.'), ('Панфиловский Э.')", 'run');
       // await queryAsyncWraper("INSERT INTO tasks (id,task_id,task_descript,task_priority,task_status,deadline,appoint_user_id,appoint_department_id,appoint_subdepartment_id,appoint_position_id,responsible_user_id,responsible_department_id,responsible_subdepartment_id,responsible_position_id,created_on,approved_on,reject_on,confirmation_on,closed_on,setResponseSubDep_on,setResponseUser_on) VALUES (5,'0e8f694a-a47e-441e-8569-91c9399c61ef','Назначить на старшего мастера','false','closed','2023-08-28',11,2,2,NULL,NULL,3,5,17,'2023-08-25 05:19:23','2023-08-25 05:20:18',NULL,'2023-12-11 07:32:08','2023-12-11 07:32:45','2023-12-11 07:32:01',NULL)")
-      await queryAsyncWraper(insertTasks)
-      await queryAsyncWraper(insertTasksFiles)
+      // await queryAsyncWraper(insertTasks)
+      // await queryAsyncWraper(insertTasksFiles)
     }
   } catch (error) {
     console.error('createTableDepartments ERROR: ', error);
@@ -457,7 +456,7 @@ const createTableTokens = async () => {
       console.log('>>>>>>>>>>>>', token)
       const command = `INSERT INTO tokens(user_id, token) VALUES (?, ?)`;
       await queryAsyncWraperParam(command, [objUser.id, token], `run`)
-      await createUsers(userDataArray)
+      // await createUsers(userDataArray)
     }
   } catch (error) {
     console.error("Error creating tokens table:", error);
@@ -531,6 +530,7 @@ const createTableVenchelFiles = async () => {
 
 
 db.serialize(async () => {
+  console.log('dddddddddddddddddddddddd')
   createTableTasks()
   createTableUsers()
   createTableTokens()
